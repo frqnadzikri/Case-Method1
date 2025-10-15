f48910064cd39dd4737bccc0f1305a1806ac0589
Furqon Adzikri <furqonadzikri311@gmail.com>
2025-10-15 21:27:32 +0700

    Konversi Nilai Matkul 1 dan 2

--- SistemKelulusan.java
+++ SistemKelulusan.java
@@ -28,7 +28,7 @@ public class SistemKelulusan {
         double uas2 = sc.nextDouble();
         System.out.print("Nilai Tugas : ");
         double tugas2 = sc.nextDouble();
-        
+
         // ===== PERHITUNGAN NILAI AKHIR =====
         double akhir1 = (uts1 * 0.3) + (uas1 * 0.4) + (tugas1 * 0.3);
         double akhir2 = (uts2 * 0.3) + (uas2 * 0.4) + (tugas2 * 0.3);
@@ -37,3 +37,31 @@ public class SistemKelulusan {
         String huruf2 = "";
         String status1 = "";
         String status2 = "";
+        
+        // ===== KONVERSI NILAI HURUF DAN STATUS MATA KULIAH 1 =====
+        if (akhir1 >= 80 && akhir1 <= 100) huruf1 = "A";
+        else if (akhir1 >= 73) huruf1 = "B+";
+        else if (akhir1 >= 65) huruf1 = "B";
+        else if (akhir1 >= 60) huruf1 = "C+";
+        else if (akhir1 >= 50) huruf1 = "C";
+        else if (akhir1 >= 39) huruf1 = "D";
+        else huruf1 = "E";
+
+        if (akhir1 >= 60)
+            status1 = "LULUS";
+        else
+            status1 = "TIDAK LULUS";
+
+        // ===== KONVERSI NILAI HURUF DAN STATUS MATA KULIAH 2 =====
+        if (akhir2 >= 80 && akhir2 <= 100) huruf2 = "A";
+        else if (akhir2 >= 73) huruf2 = "B+";
+        else if (akhir2 >= 65) huruf2 = "B";
+        else if (akhir2 >= 60) huruf2 = "C+";
+        else if (akhir2 >= 50) huruf2 = "C";
+        else if (akhir2 >= 39) huruf2 = "D";
+        else huruf2 = "E";
+
+        if (akhir2 >= 60)
+            status2 = "LULUS";
+        else
+            status2 = "TIDAK LULUS";
