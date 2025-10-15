c828df6f6acd96c5047f4321dc583ed91066129d
Furqon Adzikri <furqonadzikri311@gmail.com>
2025-10-15 21:24:11 +0700

    Input Data Mahasiswa

---
+++ SistemKelulusan.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class SistemKelulusan {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // ===== INPUT DATA MAHASISWA =====
+        System.out.println("===== INPUT DATA MAHASISWA =====");
+        System.out.print("Nama : ");
+        String nama = sc.nextLine();
+        System.out.print("NIM  : ");
+        String nim = sc.nextLine();
