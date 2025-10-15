c0340b7f0b08bbe707e596faf0c9ab61a752b343
Furqon Adzikri <furqonadzikri311@gmail.com>
2025-10-15 21:25:25 +0700

    Mata Kuliah 1 dan 2

--- SistemKelulusan.java
+++ SistemKelulusan.java
@@ -10,3 +10,21 @@ public class SistemKelulusan {
         String nama = sc.nextLine();
         System.out.print("NIM  : ");
         String nim = sc.nextLine();
+
+         // ---- Mata Kuliah 1 ----
+        System.out.println("\n--- Mata Kuliah 1: Algoritma dan Pemrograman ---");
+        System.out.print("Nilai UTS   : ");
+        double uts1 = sc.nextDouble();
+        System.out.print("Nilai UAS   : ");
+        double uas1 = sc.nextDouble();
+        System.out.print("Nilai Tugas : ");
+        double tugas1 = sc.nextDouble();
+
+        // ---- Mata Kuliah 2 ----
+        System.out.println("\n--- Mata Kuliah 2: Struktur Data ---");
+        System.out.print("Nilai UTS   : ");
+        double uts2 = sc.nextDouble();
+        System.out.print("Nilai UAS   : ");
+        double uas2 = sc.nextDouble();
+        System.out.print("Nilai Tugas : ");
+        double tugas2 = sc.nextDouble();
